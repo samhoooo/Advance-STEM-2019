242a9d923f558c50b65a077c890ba75e7ab9efd8
evnchn <37951241+evnchn@users.noreply.github.com>
2019-01-11 15:18:47 +0800

    Updated script(still not working)

--- tcp-server/TCP.py
+++ tcp-server/TCP.py
@@ -7,10 +7,8 @@ window_size = 100
 
 q = queue.Queue()
 from graphics import *
-from tkinter import *
-master = Tk()
-w = Canvas(master, width=line_width*window_size, height=line_width*window_size)
-w.pack()
+
+
 
 global x
 global y
@@ -21,20 +19,21 @@ def put_draw(dx,dy):
 def update_point(dx,dy): # i meant line, oops
 	global x
 	global y
-	w.create_line(x,y,x+line_width*dx, y+line_width*dy,fill="black", width=line_width)
-	#line = Line(Point(x,y), Point(x+line_width*dx, y+line_width*dy))
-	#line.setWidth(line_width)
-	#line.draw(win)
+	
+	'''line = Line(Point(x,y), Point(x+line_width*dx, y+line_width*dy))
+	line.setWidth(line_width)
+	line.draw(win)
+	update()
 	print('X: {}, Y: {}, NewX: {}, NewY: {}'.format(x,y,x+line_width*dx, y+line_width*dy))
-	#x = x+line_width*dx
-	#y = y+line_width*dy
-	#Point(x,y).draw(win)
+	x = x+line_width*dx
+	y = y+line_width*dy'''
 
-#win = GraphWin("Plot",line_width*window_size,line_width*window_size)
+
+
 #win.setCoords(-win.getWidth()/2,-win.getHeight()/2,win.getWidth()/2,win.getHeight()/2)
 # -window_size/2 to +window_size/2 for x and y
 
-update_point(25,0)
+##update_point(25,0)
 
 bind_ip = '0.0.0.0'
 bind_port = 9999
@@ -57,22 +56,29 @@ def loop_a():
 	while True:
 		client_sock, address = server.accept()
 		print('Accepted connection from {}:{}'.format(address[0], address[1]))
-		client_handler = threading.Thread(
-			target=handle_client_connection,
-			args=(client_sock,)
-		)
+		client_handler = threading.Thread(target=handle_client_connection,args=(client_sock,))
 		client_handler.start()
-def loop_b():
-	while True:
-		dx,dy=q.get()
-		update_point(dx,dy)
-def loop_c():
-	update()
+
+	
 p1 = threading.Thread(target=loop_a)
-p2 = threading.Thread(target=loop_b)
+
 
 
 p1.start()
-p2.start()
-mainloop()
-update_point(0,5)
+
+win = GraphWin("Plot",line_width*window_size,line_width*window_size,autoflush=False)
+
+while True:
+	dx,dy=q.get()
+	#update_point(dx,dy)
+	line = Line(Point(x,y), Point(x+line_width*dx, y+line_width*dy))
+	line.setWidth(line_width)
+	line.draw(win)
+	print('X: {}, Y: {}, NewX: {}, NewY: {}'.format(x,y,x+line_width*dx, y+line_width*dy))
+	x = x+line_width*dx
+	y = y+line_width*dy
+	update()
+
+
+
+
