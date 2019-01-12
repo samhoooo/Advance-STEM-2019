31775e71413a66497c7d2f574ff6dc2c9f230019
evnchn <37951241+evnchn@users.noreply.github.com>
2019-01-12 21:49:50 +0800

    Rewritten map extending code
    Now this one's working

--- tcp-server-v2/TCP.py
+++ tcp-server-v2/TCP.py
@@ -1,22 +1,34 @@
 import socket
 import threading
 import queue
-import array
 from colorama import *
+import os
+
+def cls():
+	os.system('cls' if os.name=='nt' else 'clear')
+
+q = queue.Queue()
+
 init(autoreset=True)
-map = [[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,99,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1]]
-x = 4
-y = 4
+map = [[1,1,1,1,1],[1,1,1,1,1],[1,1,99,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+x = y = 2
 
 xWASD = {
-        "A": -1,
-        "D": 1
+	"A": -1,
+	"D": 1
 }
 	
 yWASD = {
-        "W": -1,
-        "S": 1
-    }
+	"W": -1,
+	"S": 1
+}
+numCHAR = {
+	0:Back.RED+Fore.BLACK + "XX",
+	99:Back.GREEN + "  ",
+	1:Back.BLUE+Fore.BLACK + "[]"
+
+}
+
 
 
 bind_ip = '0.0.0.0'
@@ -24,66 +36,96 @@ bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
-server.listen(100)
+server.listen(1)
 
 print('Listening on {}:{}'.format(bind_ip, bind_port))
 
 
-def handle_client_connection(client_socket):
-	global x
-	global y
-	global xWASD
-	global yWASD
-	request = client_socket.recv(1024)
-	wasd,val1,val2,val3,val4 = tuple(request.decode("utf-8").rstrip("\n\r").split("#"))
-	print('Received WASD: {}, Values: {}, {}, {}, {}'.format(wasd,val1,val2,val3,val4))
-	
-	
-	# updating map
-	x += xWASD.get(wasd,0)
-	y += yWASD.get(wasd,0)
-	if y < 1:
-		map.insert(0,[1 for x in range(len(map[0]))])
-		y += 1
-	if (y+2) > len(map):
-		map.append([1 for x in range(len(map[0]))])
-	if x < 1:
-		for r in map:
-			r.insert(0,1)
-		x += 1
-	if (x+2) > len(map[0]):
-		for r in map:
-			r.append(1)
-	if map[y-1][x] != 99:
-		map[y-1][x]=val1
-	if map[y][x-1] != 99:
-		map[y][x-1]=val2
-	if map[y+1][x] != 99:
-		map[y+1][x]=val3
-	if map[y][x+1] != 99:
-		map[y][x+1]=val4
-	map[y][x]=99
-	# print map
-	for ir in range(len(map)):
-		for ic in range(len(map[ir])):
-			if ir==y and ic==x:
-				print(Style.BRIGHT + Fore.BLUE + "██" ,end="")
-			elif map[ir][ic] == 99:
-				print(Fore.GREEN + str(map[ir][ic]).zfill(2) ,end="")
-			elif int(map[ir][ic]) > 1:
-				print(Fore.YELLOW + str(map[ir][ic]).zfill(2) ,end="")
-			elif map[ir][ic] == 1:
-				print(Fore.BLUE + "??" ,end="")
-			else:
-				print(Fore.RED + "XX",end="")
-		print()
-	
-	
-	print('New coord: {},{}'.format(x,y))
-	
+def handle_client_connection(client_socket,ip,name):
+	q.put((*(client_socket.recv(1024).decode("utf-8").rstrip("\n\r").split("#")),ip,name))
 	client_socket.close()
-while True: 
-	client_sock, address = server.accept()
-	print('Accepted connection from {}:{}'.format(address[0], address[1]))
-	client_handler = threading.Thread(target=handle_client_connection,args=(client_sock,))
-	client_handler.start()
+
+def taskA():
+	while True: 
+		client_sock, address = server.accept()
+		threading.Thread(target=handle_client_connection,args=(client_sock,address[0], address[1],)).start()
+		#client_handler
+p1 = threading.Thread(target=taskA)
+#p2 = threading.Thread(target=taskB, args=(*args, **kwargs))
+
+p1.start()
+#p2.start()
+while True:
+	if not q.empty():
+		cls()
+		wasd,valW,valA,valS,valD,ip,addr = q.get()
+		valW = [ int(x) for x in valW.split("&")]
+		valA = [ int(x) for x in valA.split("&")]
+		valS = [ int(x) for x in valS.split("&")]
+		valD = [ int(x) for x in valD.split("&")]
+		print('{}:{}: Going: {}\nValues:\n{}\n{}\n{}\n{}'.format(ip,addr,wasd,valW,valA,valS,valD))
+		x += xWASD.get(wasd,0)
+		y += yWASD.get(wasd,0)
+		'''
+		if (y+yWASD.get(wasd,0)-len(valW)) < 0:
+			for iii in range(len(valW)-yWASD.get(wasd,0)-2):
+				map.insert(0,[1 for xxxx in range(len(map[0]))])
+				y += 1
+		if (y+yWASD.get(wasd,0)+len(valS)) > len(map):
+			for iii in range(len(valS)+yWASD.get(wasd,0)+2):
+				map.append([1 for xxxx in range(len(map))])
+		if (x+xWASD.get(wasd,0)-len(valA)) < 0:
+			for iii in range(len(valA)-xWASD.get(wasd,0)-2):
+				for r in map:
+					r.insert(0,1)
+				x += 1
+		if (x+xWASD.get(wasd,0)+len(valD)) > len(map[0]):
+			for iii in range(len(valD)-0*(x+xWASD.get(wasd,0)+len(valD))):
+				for r1 in map:
+					r1.append(1)'''
+					
+		if (y-len(valW)) < 0:
+			map = [[1 for xxxx in range(len(map[0]))] for xxxxx in range(-(y-len(valW)))]+map
+			y += -(y-len(valW))
+		if (y+len(valS)) > len(map)-1:
+		#	map.append([1 for xxxx in range(len(map)-(y+len(valS)))])
+			map = map+[[1 for xxxx in range(len(map[0]))] for xxxxx in range((y+len(valS))-len(map)+1)]
+		if (x+len(valD)) > len(map[0])-1:
+			print("trig d")
+			add = [1 for xxxx in range((x+len(valD))-len(map[0])+1)]
+			print(add)
+			for rowid in range(len(map)):
+				map[rowid] = map[rowid] + add
+				print(add)
+				print(map[rowid])
+		if (x-len(valA)) < 0:
+			add = [1 for xxxx in range(-(x-len(valA)))]
+			for rowid in range(len(map)):
+				map[rowid] = add + map[rowid]
+			x += -(x-len(valA))
+		for ggg in range(len(valW)):
+			if map[y-(ggg+1)][x] != 99:
+				map[y-(ggg+1)][x]=valW[ggg]
+		for ggg in range(len(valA)):
+			if map[y][x-(ggg+1)] != 99:
+				map[y][x-(ggg+1)]=valA[ggg]
+		for ggg in range(len(valS)):
+			if map[y+(ggg+1)][x] != 99:
+				map[y+(ggg+1)][x]=valS[ggg]
+		for ggg in range(len(valD)):
+			if map[y][x+(ggg+1)] != 99:
+				map[y][x+(ggg+1)]=valD[ggg]
+		map[y][x]=99
+		print(str(x))
+		print(str(y))
+		# print map
+		if not q.qsize():
+			for ir in range(len(map)):
+				pstr = ''
+				for ic in range(len(map[ir])):
+					num = int(map[ir][ic])
+					if ir==y and ic==x:
+						pstr += Back.CYAN + "  "
+					else:
+						pstr += numCHAR.get(num,Back.YELLOW+Fore.WHITE+str(num).zfill(2))
+				print(pstr)
