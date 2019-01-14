e3d890eedaee905977f78e7803c80f16cc302b59
evnchn <37951241+evnchn@users.noreply.github.com>
2019-01-14 18:34:10 +0800

    Old code looks ugly to me
    fixed

--- tcp-server-v2-multicar/TCP.py
+++ tcp-server-v2-multicar/TCP.py
@@ -11,9 +11,9 @@ q = queue.Queue()
 
 init(autoreset=True)
 map = [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]]
-xid = {}
-yid = {}
-spset=False
+xID = {}
+yID = {}
+spset = False
 xWASD = {
 	"A": -1,
 	"D": 1
@@ -51,12 +51,9 @@ def taskA():
 	while True: 
 		client_sock, address = server.accept()
 		threading.Thread(target=handle_client_connection,args=(client_sock,address[0], address[1],)).start()
-		#client_handler
-p1 = threading.Thread(target=taskA)
-#p2 = threading.Thread(target=taskB, args=(*args, **kwargs))
 
-p1.start()
-#p2.start()
+threading.Thread(target=taskA).start()
+
 while True:
 	if not q.empty():
 		cls()
@@ -75,46 +72,44 @@ while True:
 					break
 			if not do_loop:
 				break
-		print(ic)
-		print(ir)
-		if not id in xid:
-			xid[id] = ic
-		if not id in yid:
-			yid[id] = ir
-		xid[id] += xWASD.get(wasd,0)
-		yid[id] += yWASD.get(wasd,0)
-		print('ID:{}@{}:{}: Going: {}>({},{})\nValues:\n{}\n{}\n{}\n{}'.format(id,ip,addr,wasd,xid[id],yid[id],valW,valA,valS,valD))
-		if (yid[id]-len(valW)) < 0:
-			map = [[1 for xxxx in range(len(map[0]))] for xxxxx in range(-(yid[id]-len(valW)))]+map
-			yid[id] -= yid[id]-len(valW)
-		if (yid[id]+len(valS)) > len(map)-1:
-			map += [[1 for xxxx in range(len(map[0]))] for xxxxx in range((yid[id]+len(valS))-len(map)+1)]
-		if (xid[id]+len(valD)) > len(map[0])-1:
-			add = [1 for xxxx in range((xid[id]+len(valD))-len(map[0])+1)]
+		if not id in xID:
+			xID[id] = ic
+		if not id in yID:
+			yID[id] = ir
+		xID[id] += xWASD.get(wasd,0)
+		yID[id] += yWASD.get(wasd,0)
+		print('ID:{}@{}:{}: Going: {}>({},{})\nValues:\n{}\n{}\n{}\n{}'.format(id,ip,addr,wasd,xID[id],yID[id],valW,valA,valS,valD))
+		if (yID[id]-len(valW)) < 0:
+			map = [[1 for i in range(len(map[0]))] for j in range(-(yID[id]-len(valW)))]+map
+			yID[id] -= yID[id]-len(valW)
+		if (yID[id]+len(valS)) > len(map)-1:
+			map += [[1 for i in range(len(map[0]))] for j in range((yID[id]+len(valS))-len(map)+1)]
+		if (xID[id]+len(valD)) > len(map[0])-1:
+			add = [1 for i in range((xID[id]+len(valD))-len(map[0])+1)]
 			for rowid in range(len(map)):
 				map[rowid] += add
-		if (xid[id]-len(valA)) < 0:
-			add = [1 for xxxx in range(-(xid[id]-len(valA)))]
+		if (xID[id]-len(valA)) < 0:
+			add = [1 for i in range(-(xID[id]-len(valA)))]
 			for rowid in range(len(map)):
 				map[rowid] = add + map[rowid]
-			xid[id] = xid[id]-(xid[id]-len(valA))
+			xID[id] = xID[id]-(xID[id]-len(valA))
 		if not spset:
-			map[yid[id]][xid[id]]=99
+			map[yID[id]][xID[id]]=99
 			spset=True
 		else:
-			map[yid[id]][xid[id]]=98
-		for ggg in range(len(valW)):
-			if map[yid[id]-(ggg+1)][xid[id]] < 98:
-				map[yid[id]-(ggg+1)][xid[id]]=valW[ggg]
-		for ggg in range(len(valA)):
-			if map[yid[id]][xid[id]-(ggg+1)] < 98:
-				map[yid[id]][xid[id]-(ggg+1)]=valA[ggg]
-		for ggg in range(len(valS)):
-			if map[yid[id]+(ggg+1)][xid[id]] < 98:
-				map[yid[id]+(ggg+1)][xid[id]]=valS[ggg]
-		for ggg in range(len(valD)):
-			if map[yid[id]][xid[id]+(ggg+1)] < 98:
-				map[yid[id]][xid[id]+(ggg+1)]=valD[ggg]
+			map[yID[id]][xID[id]]=98
+		for i in range(len(valW)):
+			if map[yID[id]-(i+1)][xID[id]] < 98:
+				map[yID[id]-(i+1)][xID[id]]=valW[i]
+		for i in range(len(valA)):
+			if map[yID[id]][xID[id]-(i+1)] < 98:
+				map[yID[id]][xID[id]-(i+1)]=valA[i]
+		for i in range(len(valS)):
+			if map[yID[id]+(i+1)][xID[id]] < 98:
+				map[yID[id]+(i+1)][xID[id]]=valS[i]
+		for i in range(len(valD)):
+			if map[yID[id]][xID[id]+(i+1)] < 98:
+				map[yID[id]][xID[id]+(i+1)]=valD[i]
 		
 
 		if not q.qsize():
@@ -122,7 +117,7 @@ while True:
 				pstr = ''
 				for ic in range(len(map[ir])):
 					num = int(map[ir][ic])
-					if ir==yid[id] and ic==xid[id]:
+					if ir==yID[id] and ic==xID[id]:
 						pstr += Back.CYAN + "  "
 					else:
 						pstr += numCHAR.get(num,Back.YELLOW+Fore.WHITE+str(num).zfill(2))
